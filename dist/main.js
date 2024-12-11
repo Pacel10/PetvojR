431fd32f5f46ce6f699572b627ad8e2e05d7df76
Pacel10 <vlastikcze@gmail.com>
2024-12-11 16:35:55 +0100

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    root.textContent = "Hello, GitHub Pages!";
+    root.textContent = "Deployed to GitHub Pages!";
 });
